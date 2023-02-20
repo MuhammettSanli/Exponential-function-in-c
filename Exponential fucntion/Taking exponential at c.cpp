13ac5eccb9cdf803adf2e7797c05e6917337376f
MuhammetSanli <114609160+MuhammettSanli@users.noreply.github.com>
2023-02-20 14:03:14 +0300

    Add files via upload

---
+++ Exponential fucntion/Taking exponential at c.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main() {
+	
+	int number,expont,result;
+	
+	printf("Write a number");
+	scanf("%d",&number);
+	printf("Write a expont");
+	scanf("%d",&expont);
+	
+	result=pow(number,expont);
+	printf("Result=%d",result);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
